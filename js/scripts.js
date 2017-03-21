97e34d4e9b335e18b72aa24feb451348733c15c1
Maciej Czerwinski <maciejcz@wp.pl>
2017-03-21 21:27:24 +0100

    8.4

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var a = prompt("Podaj dlugosc podstawy trojkata"),
+	h = prompt("Podaj wysokosc trojkata"),
+	triangleArea = a * h / 2;
+
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea); 
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea); 
